fbdfe38c3087b6cc529e065bcc2beb8096971a2a
Micah van de Merwe <micahvdm@gmail.com>
2024-08-18 18:37:09 +0200

    wifi fixes

--- src/NetworkManager/NetworkManagerPlugin.cpp
+++ src/NetworkManager/NetworkManagerPlugin.cpp
@@ -1,5 +1,6 @@
 #include "NetworkManagerPlugin.h"
 #include "NetworkManager.h"
+#include <QtQml>
 
 int NetworkManagerPlugin::unused_val = qmlRegisterType<NetworkManager>("NetworkManager", 1, 0, "NetworkManager");
 // void NetworkManagerPlugin::registerTypes(const char *uri) {
